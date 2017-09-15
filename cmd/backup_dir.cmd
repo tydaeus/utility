272a5ccd376285f1037752ab57a0f413d764573d
David Ellis <tydaeus@gmail.com>
2017-09-15 13:28:54 -0500

    create backup_dir.cmd

---
+++ cmd/backup_dir.cmd
@@ -0,0 +1,36 @@
+@Echo off
+setLocal enableDelayedExpansion
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BACKUP_DIR
+::
+:: Usage:
+::      BACKUP_DIR TARGET_DIR
+::
+:: Renames TARGET_DIR to TARGET_DIR.bak, removing any pre-existing 
+:: TARGET_DIR.bak. TARGET_DIR must be named with full filepath, wrapped in
+:: quotes if any spaces exist in path or dirname.
+::
+:: TODO: proper error handling
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+set ERRLEVEL=0
+set TARGET_DIR=%~1
+set TARGET_NAME=%~nx1
+
+if exist "%TARGET_DIR%.bak" (
+    rd /S /Q "%TARGET_DIR%.bak" || goto :BACKUP_DIR_ERR
+)
+
+if exist "%TARGET_DIR%" (
+    move "%TARGET_DIR%" "%TARGET_DIR%.bak" > nul || goto :BACKUP_DIR_ERR
+)
+
+goto :BACKUP_DIR_END
+
+:BACKUP_DIR_ERR
+set ERRLEVEL=1
+echo ERROR: failed to backup %TARGET_DIR%
+:BACKUP_DIR_END
+endLocal & set ERRLEVEL=%ERRLEVEL%
+exit /b %ERRLEVEL%
