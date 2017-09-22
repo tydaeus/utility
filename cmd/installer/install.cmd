d7286b8e34122d9dc2423fb1b822530f2534659c
David Ellis <tydaeus@gmail.com>
2017-09-22 18:04:13 -0500

    begin work on installer

---
+++ cmd/installer/install.cmd
@@ -0,0 +1,38 @@
+@Echo Off
+setLocal enableDelayedExpansion
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: install
+::
+:: Provides simple installer utility, by using util scripts.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+set ERRLVL=0
+set ERRMSG=
+set SCRIPT_DIR=%~dp0
+
+::-----------------------------------------------------------------------------
+:: setup the path to util scripts
+::
+:: precedence:
+::  1. in script's dir
+::  2. in script's \util subdir
+::  3. in script's parent dir's \util subdir
+::  4. at CMD_UTIL_HOME
+::-----------------------------------------------------------------------------
+
+if not "%CMD_UTIL_HOME%"=="" set PATH=%CMD_UTIL_HOME%;%PATH%
+if exist "%SCRIPT_DIR%"..\util\* set PATH=%SCRIPT_DIR%..\util\;%PATH%
+if exist "%SCRIPT_DIR%"\util\* set PATH=%SCRIPT_DIR%util\;%PATH%
+set PATH=%SCRIPT_DIR%
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:ERR
+if "%ERRMSG%"=="" set ERRMSG=unknown error occurred
+if "%ERRLEV%"=="0" set ERRLEV=1
+goto :END
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:END
+endLocal & set ERRLEV=%ERRLEV% & set ERRMSG=%ERRMSG%
+exit /b %ERRLEV%
