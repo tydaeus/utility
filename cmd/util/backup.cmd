44cc0066fe3e302007424c6623a5ad018ddbbcbe
David Ellis <tydaeus@gmail.com>
2017-10-02 17:30:38 -0500

    use BACKUP_HOME for backups, if defined

--- cmd/util/backup.cmd
+++ cmd/util/backup.cmd
@@ -7,7 +7,9 @@ setLocal enableDelayedExpansion
 :: Usage:
 ::      backup TARGET
 ::
-:: Makes a copy of TARGET as [timestamp]TARGET.bak.
+:: Makes a copy of TARGET as [timestamp]TARGET.bak. This backup will be located
+:: at BACKUP_HOME if defined, target's parent directory otherwise.
+::
 :: Does nothing if TARGET does not exist.
 ::
 :: Depends on several of the utility commands.
@@ -17,6 +19,8 @@ set ERRLEVEL=0
 set TARGET=%~1
 set TARGET_PATH=%~dp1
 set TARGET_NAME=%~nx1
+
+if defined BACKUP_HOME set "TARGET_PATH=%BACKUP_HOME%\"
 
 call eval "short_date" SDATE
 call eval "short_time" STIME
