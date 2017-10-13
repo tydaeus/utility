67609cea6a301421a0a8489264fc8196d0fba6df
David Ellis <tydaeus@gmail.com>
2017-10-13 10:25:58 -0500

    report error if no files found; formatting and standardization

--- cmd/util/get_latest_file.cmd
+++ cmd/util/get_latest_file.cmd
@@ -1,6 +1,7 @@
 @Echo off
 setLocal enableDelayedExpansion
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: get_latest_file
 ::
 :: Usage:
@@ -12,12 +13,27 @@ setLocal enableDelayedExpansion
 :: Returns the most recently modified file in PATH whose name matches 
 :: FILE_NAME as RET. Note that FILE_NAME is processed using findstr, so
 :: some basic regex can be used.
-
+::
+:: Sets ERRLEV and ERRORLEVEL to 1 if no files matched.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 set DIR_PATH=%~1
 set FILE_NAME=%~2
+set NEWEST=
+set RET=
+set ERRLEV=0
 
 for /f "tokens=*" %%A in ('dir /b /od "%DIR_PATH%" ^| findstr "%FILE_NAME%"') do set NEWEST=%DIR_PATH%\%%~A
 
-set RET=%NEWEST%
+if not ["%NEWEST%"]==[""] goto :SUCCESS
 
-endLocal & set RET=%RET%
+:ERR
+set ERRLEV=1
+goto :END
+
+:SUCCESS
+set RET=%NEWEST%
+goto :END
+
+:END
+endLocal & set "RET=%RET%" & set ERRLEV=%ERRLEV%
+exit /b %ERRLEV%
