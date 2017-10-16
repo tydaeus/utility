b17dcc2c0f4f272d7043983c3aa518fc8862e6fa
David Ellis <tydaeus@gmail.com>
2017-10-16 17:10:33 -0500

    functional matching with optional output var specification

--- cmd/util/match.cmd
+++ cmd/util/match.cmd
@@ -5,7 +5,7 @@ goto :INIT
 ::-----USAGE-------------------------------------------------------------------
 :DISPLAY_USAGE_MESSAGE
 echo: Usage:
-echo:   %SCRIPT_NAME% [--output=OUTPUT] STRING PATTERN
+echo:   %SCRIPT_NAME% [--output:OUTPUT] STRING PATTERN
 exit /b
 
 ::-----DISPLAY_HELP------------------------------------------------------------
@@ -21,7 +21,8 @@ echo: Determines whether STRING matches PATTERN. Note that special character
 echo: escaping has not been fully implemented.
 echo:
 echo: Returns:
-echo:   sets RET=1 if STRING matches, 0 if STRING does not match
+echo:   1 if STRING matches, 0 if STRING does not match. This will be assigned to
+echo:   variable named in OUTPUT, if specified; sent to stdout otherwise.
 echo:
 exit /b
 
@@ -34,8 +35,13 @@ set DISPLAY_HELP=0
 set USAGE_ERR=0
 set STRING=
 set PATTERN=
+set OUTPUT_VAR=
 
-call :PROCESS_ARGS %*
+call split_flags %*
+
+call :PROCESS_ARGS %ARGS%
+call :PROCESS_SIMPLE_FLAGS %SIMPLE_FLAGS%
+call :PROCESS_LONG_FLAGS %LONG_FLAGS%
 
 if [%DISPLAY_HELP%]==[1] (
 	call :DISPLAY_HELP
@@ -48,18 +54,23 @@ if [%USAGE_ERR%]==[1] (
 	goto :ERR
 )
 
-set "STRING=%~1"
-set "PATTERN=%~2"
-
-call :MATCH_SUB "%STRING%" "%PATTERN%"
+call :MATCH_SUB "%STRING%" "%PATTERN%" || goto :ERR
+goto :SUCCESS
 
 :ERR
 if "%ERRLEV%"=="0" set ERRLEV=1
 goto :END
 
+:SUCCESS
+if ["%OUTPUT_VAR%"]==[""] echo:%RET%
+goto :END
+
 :END
-echo %RET%
-endLocal & set ERRLEV=%ERRLEV%
+if ["%OUTPUT_VAR%"]==[""] (
+	endLocal & set ERRLEV=%ERRLEV%
+) else (
+	endLocal & set %OUTPUT_VAR%=%RET%
+)
 exit /b %ERRLEV%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -69,46 +80,15 @@ exit /b %ERRLEV%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :PROCESS_ARGS
 
-:: TODO: simplify, add --output processing
+:: TODO: multi-patterns?
 
-:LOOP_UNTIL_STRING
 if [%~1]==[] goto :END_PROCESS_ARGS
-call :MATCH_SUB "%~1" "^-"
-if [%RET%]==[1] (
-	call :PROCESS_FLAG "%~1"
-	shift
-	goto :LOOP_UNTIL_STRING
-) else (
-	set "STRING=%~1"
-	shift
-	goto :LOOP_UNTIL_PATTERN
-)
+set "STRING=%~1"
+shift
 
-
-:LOOP_UNTIL_PATTERN
 if [%~1]==[] goto :END_PROCESS_ARGS
-call :MATCH_SUB "%~1" "^-"
-if [%RET%]==[1] (
-	call :PROCESS_FLAG "%~1"
-	shift
-	goto 
-) else (
-	set "PATTERN=%~1"
-	shift
-	goto :LOOP_UNTIL_END
-)
-
-:LOOP_UNTIL_END
-if [%~1]==[] goto :END_PROCESS_ARGS
-call :MATCH_SUB "%~1" "^-"
-if [%RET%]==[1] (
-	call :PROCESS_FLAG "%~1"
-	shift
-	goto 
-) else (
-	set "USAGE_ERR=1"
-	goto :END_PROCESS_ARGS
-)
+set "PATTERN=%~1"
+shift
 
 :END_PROCESS_ARGS
 if [%STRING%]==[] set USAGE_ERR=1
@@ -116,25 +96,62 @@ if [%PATTERN%]==[] set USAGE_ERR=1
 exit /b
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: PROCESS_FLAG
+:: PROCESS_SIMPLE_FLAGS
 ::
-:: Examines a flag to determine appropriate response.
+:: Examines the set of simple flags to determine appropriate response
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:PROCESS_FLAG
+:PROCESS_SIMPLE_FLAGS
+
+:: no flags
+if "%*"=="" exit /b
+
+:: check for invalid flags
+call :MATCH_SUB "%*" "[^?hH]"
+if "%RET%"=="1" (
+	set USAGE_ERR=1
+	exit /b
+)
+
+:: check for help flags
+call :MATCH_SUB "%*" "[?hH]"
+if "%RET%"=="1" (
+	set DISPLAY_HELP=1
+)
+
+exit /b
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROCESS_LONG_FLAGS
+::
+:: Iterates through the set of long flags to determine appropriate config
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:PROCESS_LONG_FLAGS
+
+:WHILE_LONG_FLAG
+if ["%~1"]==[""] goto :END_PROCESS_LONG_FLAGS
+call :PROCESS_LONG_FLAG "%~1"
+shift
+goto :WHILE_LONG_FLAG
+
+:END_PROCESS_LONG_FLAGS
+exit /b
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROCESS_LONG_FLAG
+::
+:: Checks a single long flag to determine appropriate config
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:PROCESS_LONG_FLAG
 set "FLAG=%~1"
 
-echo:flag detected as %FLAG%
-if [%FLAG%]==[-?] (
-	echo help flag found
+if ["%FLAG%"]==["help"] (
 	set DISPLAY_HELP=1
 	exit /b
 )
-if [%FLAG%]==[-h] (
-	set DISPLAY_HELP=1
-	exit /b
-)
-if ["%FLAG%"]==["--help"] (
-	set DISPLAY_HELP=1
+
+call :MATCH_SUB "%FLAG%" "^output:"
+if "%RET%"=="1" (
+	set OUTPUT_VAR=%FLAG:~7%
 	exit /b
 )
 
@@ -157,17 +174,14 @@ set "STRING=%~1"
 set "PATTERN=%~2"
 set "PATTERN=%PATTERN:^^=^%"
 
-echo %STRING% | findstr /R "%PATTERN%" > nul
+echo:%STRING%| findstr /R "%PATTERN%" > nul
 set FINDSTR_RET=%ERRORLEVEL%
-echo FINDSTR_RET: %FINDSTR_RET%
 
 if "%FINDSTR_RET%"=="0" (
 	set RET=1
 ) else (
 	set RET=0
 )
-
-echo MATCH_SUB RET: %RET%
 
 endLocal & set RET=%RET%
 exit /b
