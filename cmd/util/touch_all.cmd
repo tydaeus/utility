4707020b984b6ffdd1d1e36a2fcc2329ed8de731
David Ellis <tydaeus@gmail.com>
2017-10-19 12:18:24 -0500

    error messaging; auto-detect implementation to use

--- cmd/util/touch_all.cmd
+++ cmd/util/touch_all.cmd
@@ -6,26 +6,37 @@ setLocal enableDelayedExpansion
 set ERRLEV=0
 set TARGET_DIR=%~1
 
+if defined ProgramFiles(x86) (
+    goto :64bit
+) else (
+    goto :32bit
+)
+
+:64bit
 :: no XP support
 for /R "%TARGET_DIR%" %%A in (*) do (
     copy /b "%%A"+,, "%TARGET_DIR%" > nul
     set ERRLEV=%ERRORLEVEL%
-    if not "%ERRLEV%" == "0" goto :ERR
+    if not "%ERRLEV%"=="0" goto :ERR
 )
+goto :END
 
-:: Version with XP support:
-:: pushd "%TARGET_DIR%"
-:: for /R "%TARGET_DIR%" %%A in (*) do (
-::     copy /b "%%A"+,, > nul
-::     set ERRLEV=%ERRORLEVEL%
-::     if not "%ERRLEV%" == "0" goto :ERR
-:: )
-:: popd
-
+:32bit
+:: XP supported
+pushd "%TARGET_DIR%"
+for /R "%TARGET_DIR%" %%A in (*) do (
+    copy /b "%%A"+,, > nul
+    set ERRLEV=%ERRORLEVEL%
+    if not "%ERRLEV%"=="0" goto :ERR
+)
+popd
+goto :END
 
 goto :END
 
 :ERR
+if "%ERRLEV%"=="0" set ERRLEV=1
+echo:ERR: failed to touch all files in %TARGET_DIR% 1>&2
 :END
 endLocal & set ERRLEV=%ERRLEV%
 exit /b %ERRLEV%
