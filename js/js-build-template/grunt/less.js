0789c8ba87ed6c369313ff38bbe529c4dadc4030
tydaeus <tydaeus@gmail.com>
2017-11-16 22:24:15 -0600

    partial progress toward building bootstrap css

---
+++ js/js-build-template/grunt/less.js
@@ -0,0 +1,11 @@
+/*
+    these are the tasks used to build bootstrap's css in bootstrap's gruntfile; need to migrate into project
+grunt.registerTask('less-compile', ['less:compileCore', 'less:compileTheme']);
+grunt.registerTask('dist-css', ['less-compile', 'autoprefixer:core', 'autoprefixer:theme', 'csscomb:dist',
+    'cssmin:minifyCore', 'cssmin:minifyTheme']);
+ */
+
+
+module.exports = {
+
+};
