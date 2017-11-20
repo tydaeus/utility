3df9a1948c7d45b00096ea2e18001179d59568be
tydaeus <tydaeus@gmail.com>
2017-11-19 23:56:48 -0600

    Revert "add loadash"
    This reverts commit 6df5b7935c7271f82392f259b16e7d7b45e9a56b.

--- js/js-build-template/grunt/copy.js
+++ js/js-build-template/grunt/copy.js
@@ -1,47 +1,12 @@
-'use strict';
-
-var _ = require('lodash');
-
-module.exports = {};
-
-// list each app's name here TODO get this list from Gruntfile
-var apps = ['demo'];
-
-// copy core files for each app
-_.each(apps, function(app) {
-    module.exports[app] = {
-        // each app's index file(s)
+module.exports = {
+    less: {
         files: [
             {
                 expand: true,
-                src: ['<%= source %>/apps/' + app + '/*.index.html'],
-                dest: '<%= buildDir %>/apps/' + app,
+                src: ['node_modules/bootstrap/less/*'],
+                dest: '<%= buildDir %>/styles/less/bootstrap',
                 flatten: true
             }
         ]
-    };
-});
-
-module.exports.less = {
-    files: [
-        {
-            expand: true,
-            src: ['node_modules/bootstrap/less/*'],
-            dest: '<%= buildDir %>/styles/less/bootstrap',
-            flatten: true
-        }
-    ]
-};
-
-// copy post-build files into <%= publishDir %>
-module.exports.publish = {
-    files: [
-        // TODO generate description for each app
-        {
-            expand: true,
-            src: ['<%= buildDir %>/**'],
-            dest: '<%= publishDir %>/',
-            flatten: true
-        }
-    ]
+    }
 };
