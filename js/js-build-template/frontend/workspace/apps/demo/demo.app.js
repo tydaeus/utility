83efe2400491325d2242c255c44952d13156d1f8
tydaeus <tydaeus@gmail.com>
2017-11-25 18:09:07 -0600

    use sample module; more comments; require jquery before angular

--- js/js-build-template/frontend/workspace/apps/demo/demo.app.js
+++ js/js-build-template/frontend/workspace/apps/demo/demo.app.js
@@ -1,15 +1,25 @@
 'use strict';
 
+// require non-angular library dependencies first
+var _ = require('lodash');
+// require jquery before angular to use full jquery instead of angular's jqLite
+var $ = require('jquery');
+
+// require angular and all angular libraries before requiring/declaring custom angular
 var angular = require('angular');
 
-console.info('demo.app.js run');
+// require modules that will be used by app before declaring app
+require('../../modules/test/test.module');
 
-var demoApp = angular.module('demoApp', []);
+// must declare angular app module before adding/requiring any parts of this module
+var demoApp = angular.module('demoApp', ['test']);
 
-demoApp.controller('demoController', function($scope, $interval) {
+demoApp.controller('demoController', function($scope, $interval, tester) {
     $scope.i = 1;
 
     $interval(function() {
         $scope.i++;
     }, 500);
+
+    tester();
 });
