b657c105df8455941df76f0c4b38203b4182e50f
David Ellis <tydaeus@gmail.com>
2017-11-28 18:21:37 -0600

    start implementing file upload tool

--- js/js-build-template/frontend/workspace/apps/demo/page2/page2.controller.js
+++ js/js-build-template/frontend/workspace/apps/demo/page2/page2.controller.js
@@ -3,6 +3,37 @@
 require('angular')
     .module('demo')
     .controller('page2Controller', [
-        function () {
+        '$scope',
+        function ($scope) {
+            $scope.loadFiles = function() {
+                var elem = document.createElement('input');
+                elem.setAttribute('type', 'file');
+                elem.setAttribute('style', 'display:none');
+                elem.setAttribute('multiple', 'true');
+                // can setAttribute 'accept' to a comma-separated list of mime types or extensions to restrict
+                elem.onchange = function() { filesSelected(elem.files); };
+                // document.appendChild(elem);
+                elem.click();
+                // document.removeChild(elem);
+            };
 
+            function filesSelected(files) {
+                console.info(files);
+            }
+
+            // todo: read uploaded files
+            function readFile(file) {
+                var fileReader = new FileReader();
+                fileReader.onloadend = function(e) {
+                    deferred.resolve(e.target.result);
+                };
+
+                if (!method || method === "arrayBuffer") {
+                    fileReader.readAsArrayBuffer(file);
+                } else if (method === "text") {
+                    fileReader.readAsText(file);
+                } else if (method === "dataURL") {
+                    fileReader.readAsDataURL(file);
+                }
+            }
         }]);
