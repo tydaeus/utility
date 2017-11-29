e4d7deaa821bf2846a006ba5baa39b9b23b2cfab
David Ellis <tydaeus@gmail.com>
2017-11-29 15:47:27 -0600

    scripts start in default output_mode instead of inheriting

--- cmd/cmd_util/init_simplescript.cmd
+++ cmd/cmd_util/init_simplescript.cmd
@@ -15,12 +15,15 @@ set "PATH=%UTIL_HOME%;%PATH%"
 
 if exist "%UTIL_HOME%..\cmd_util_ext\" set "PATH=%UTIL_HOME%..\cmd_util_ext;%PATH%"
 
+:: provide calculated script vars' starting values
 set "CMD[ScriptHome]=%SCRIPT_HOME%"
 set "CMD[ScriptName]=%SCRIPT_NAME%"
 set "CMD[UtilHome]=%UTIL_HOME%"
-
 :: future development may pass root in from elsewhere
 if "%CMD[Root]%"=="" set "CMD[Root]=C:\"
+
+:: provide default config options
+set "SCRIPT_CONFIG[OUTPUT_MODE]=DEFAULT"
 
 call interpret_file "%SCRIPT_FILE%" --startToken:StartSimpleScript
 set "ERRLEV=%ERRORLEVEL%"
