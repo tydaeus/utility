639386e82af566dad89474d977ae460cc77b454f
tydaeus <tydaeus@gmail.com>
2017-11-30 22:36:19 -0600

    provide a button for saving all modified images

--- js/js-build-template/frontend/workspace/apps/demo/page2/page2.controller.js
+++ js/js-build-template/frontend/workspace/apps/demo/page2/page2.controller.js
@@ -4,6 +4,7 @@ var pickFiles = require('../../../modules/browser-files/pick-files');
 var readFiles = require('../../../modules/browser-files/read-files');
 var resizeImage = require('../../../modules/browser-files/resize-image');
 var saveFile = require('../../../modules/browser-files/save-file');
+var _ = require('underscore');
 
 require('angular')
     .module('demo')
@@ -38,10 +39,21 @@ require('angular')
             }
 
             $scope.saveFile = function(file) {
-                saveFile({
-                    data: file.imageData,
-                    name: file.name
+                new Promise(function(resolve, reject) {
+                    saveFile({
+                        data: file.imageData,
+                        name: file.name
+                    });
+                    resolve();
                 });
-            }
+            };
+
+            // note that this has the unfortunate side effect of opening multiple save dialogs at once if user's
+            // settings specify to ask where to save each file
+            $scope.saveAllFiles = function () {
+                _($scope.files).each(function(file) {
+                    $scope.saveFile(file);
+                });
+            };
 
         }]);
