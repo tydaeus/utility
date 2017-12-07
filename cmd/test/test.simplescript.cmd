f1439ea8b956dd1dac349d48c35b35aff0bf8879
David Ellis <tydaeus@gmail.com>
2017-12-07 11:38:39 -0600

    update test folder

--- cmd/test/test.simplescript.cmd
+++ cmd/test/test.simplescript.cmd
@@ -2,16 +2,26 @@
 
 StartSimpleScript
 
+StartLog "{ScriptHome}test.log"
+
 echo SimpleScript has started!
 
 echo ScriptHome: {ScriptHome}
 echo ScriptName: {ScriptName}
 echo UtilHome: {UtilHome}
 echo Root: {Root}
+echo Something with (parentheses)
 
 export BACKUP_HOME {ScriptHome}
 backup cmd_foo.cmd
 
-filter file.txt --omit:".*\.xml" filtered_file.txt
+filter file.txt filtered_file.tmp.txt --omit:"foo.txt"
+
+wait 5
+
+set R {ReturnValue}
+echo ReturnValue: {R}
 
 echo SimpleScript Complete
+
+StopLog
