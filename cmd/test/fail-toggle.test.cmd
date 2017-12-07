f52ac82ddad4f0c9822b218540add6fd8397eab5
David Ellis <tydaeus@gmail.com>
2017-12-07 10:26:37 -0600

    document pre-invocation setup

--- cmd/test/fail-toggle.test.cmd
+++ cmd/test/fail-toggle.test.cmd
@@ -1,6 +1,7 @@
 @Echo Off
 
 :: alternately succeeds or fails; first call will fail
+:: set fail-toggle.test to 0 before calling to trigger failure, 1 to trigger success
 
 if not defined fail-toggle.test set fail-toggle.test=0
 
