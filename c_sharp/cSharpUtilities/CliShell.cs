5edf0ef9945fb3c08cdad1891811d046f8fce261
tydaeus <tydaeus@gmail.com>
2018-01-08 22:15:59 -0600

    initial skeleton

---
+++ c_sharp/cSharpUtilities/CliShell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpUtilities
+{
+    public class CliShell
+    {
+        private Dictionary<string, CliCommand> commands = new Dictionary<string, CliCommand>();
+    }
+
+    public abstract class CliCommand
+    {
+        protected string name;
+
+        public string Name { get; }
+
+        public abstract string Action(List<string> arguments);
+    }
+}
