c1ca461d3e7aabf88cf83f002ab3f5b1eb460137
David Ellis <5482721+tydaeus@users.noreply.github.com>
2018-04-12 18:09:59 -0500

    allow specifying output variable

--- cmd/cmd_util/compare_versions.cmd
+++ cmd/cmd_util/compare_versions.cmd
@@ -7,18 +7,34 @@ goto :INIT
 :: Compares two version number strings to determine if the first is earlier,
 :: the same, or later. Note that these strings are assumed to be numeric, with
 :: dot or space separation. Other characters may cause unexpected results.
+:: 
+:: Options:
+::  --output:VAR_NAME
+::      Output will be stored in VAR_NAME instead of echoed to stdout.
 ::
 :USAGE
 echo: Usage:
-echo:   compare_versions VERSION_A VERSION_B
+echo:   compare_versions [--output:VAR_NAME] VERSION_A VERSION_B
 exit /b
 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :INIT
 set "ERRLEV=0"
-set "SCRIPTNAME=%~0n"
+set "SCRIPTNAME=%~n0"
+set "OUTPUT_VAR="
 
+:: parse any command-line options
+:WHILE_OPTIONS
+set "CUR_PARAM=%~1"
+if not defined CUR_PARAM goto :USAGE_ERR
+if not "!CUR_PARAM:~0,1!"=="-" goto :READ_PARAMS
+call :PARSE_OPTION "!CUR_PARAM!" || goto :USAGE_ERR
+shift
+goto :WHILE_OPTIONS
+
+:: read the required params
+:READ_PARAMS
 set "VERSION_A=%~1"
 set "VERSION_B=%~2"
 set "COMPARATOR="
@@ -31,7 +47,7 @@ set "VERSION_A=!VERSION_A:.= !"
 set "VERSION_B=!VERSION_B:.= !"
 
 :: Loop through the numbers in the version strings
-:WHILE_NUMBERS
+:DO_WHILE_NUMBERS
 call :SHIFTV VERSION_A
 call :SHIFTV VERSION_B
 
@@ -55,11 +71,11 @@ if not defined B set "B=0"
 set /a "COMPARATOR=A-B"
 
 :: we're done if A and B are not equal; must continue if they are
-if !COMPARATOR! EQU 0 goto :WHILE_NUMBERS
+if !COMPARATOR! EQU 0 goto :DO_WHILE_NUMBERS
 
 :END_WHILE_NUMBERS
-echo:!COMPARATOR!
-
+if not defined OUTPUT_VAR echo:!COMPARATOR!
+if defined OUTPUT_VAR set "!OUTPUT_VAR!=!COMPARATOR!"
 goto :END
 
 :USAGE_ERR
@@ -73,7 +89,11 @@ echo:ERROR: !SCRIPTNAME! failed 1>&2
 goto :END
 
 :END
-endLocal & set "ERRLEV=%ERRLEV%"
+if not defined OUTPUT_VAR (
+    endLocal & set "ERRLEV=%ERRLEV%"
+) else (
+    endLocal & set "ERRLEV=%ERRLEV%" & set "%OUTPUT_VAR%=%COMPARATOR%"
+)
 exit /b %ERRLEV%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -129,3 +149,19 @@ goto :END
 
 :SHIFTV_END
 exit /b %ERRLEV%
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PARSE_OPTION
+::
+:: Parses a command-line option. Currently only --output:VAR_NAME is supported.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:PARSE_OPTION
+set "OPTION=%~1"
+if not "!OPTION:~0,9!"=="--output:" (
+    echo:ERROR:Unrecognized option: !OPTION! 1>&2
+    exit /b 1
+)
+
+set "OUTPUT_VAR=!OPTION:~9!"
+
+exit /b
