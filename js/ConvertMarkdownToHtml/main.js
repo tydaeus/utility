7d75921ca3f654164e6d2fdd0ce497332eb56237
tydaeus <email>
2019-05-29 16:22:14 -0500

    enhance: convert all specified files, instead of just the first

--- js/ConvertMarkdownToHtml/main.js
+++ js/ConvertMarkdownToHtml/main.js
@@ -3,60 +3,57 @@ const path = require('path');
 const fs = require('fs');
 
 let inputFile, outputFile;
-if (process.argv.length > 2) {
-     inputFile = process.argv[2];
-}
 
-if (process.argv.length > 3) {
-    outputFile = process.argv[3];
-}
-
-if (!outputFile) {
+for (let i = 2; i < process.argv.length; i++) {
+    inputFile = process.argv[i];
     outputFile = inputFile + '.html';
-    console.info('No outputFilePath specified, defaulting to: ' + outputFile);
+    convertMarkdownFile(inputFile, outputFile);
 }
 
-console.info('Reading markdown from "' + inputFile + '"');
+function convertMarkdownFile(inputFile, outputFile) {
+    console.info('Reading markdown from "' + inputFile + '"');
 
-let inputContent = fs.readFileSync(inputFile, {encoding: 'utf8'});
+    let inputContent = fs.readFileSync(inputFile, {encoding: 'utf8'});
 
-// convert markdown to html
-let reader = new commonmark.Parser();
-let writer = new commonmark.HtmlRenderer();
-let parsed = reader.parse(inputContent);
-let outputBody = writer.render(parsed);
+    // convert markdown to html
+    let reader = new commonmark.Parser();
+    let writer = new commonmark.HtmlRenderer();
+    let parsed = reader.parse(inputContent);
+    let outputBody = writer.render(parsed);
 
-// read css file
-// FUTURE: allow specifying alternative css file
-// FUTURE: allow linking css file instead of embedding?
-let css = fs.readFileSync(path.join(__dirname, 'assets/github-markdown.css'));
+    // read css file
+    // FUTURE: allow specifying alternative css file
+    // FUTURE: allow linking css file instead of embedding?
+    let css = fs.readFileSync(path.join(__dirname, 'assets/github-markdown.css'));
 
-// create header
-let outputHeader =
-    '<style>\n' +
-    css +
-    '.markdown-body { \n' +
-    '    box-sizing: border-box; \n' +
-    '    min-width: 200px; \n' +
-    '    max-width: 980px; \n' +
-    '    margin: 0 auto; \n' +
-    '    padding: 45px; \n' +
-    '} \n' +
-    '\n' +
-    '@media (max-width: 767px) { \n' +
-    '.markdown-body { \n' +
-    '        padding: 15px; \n' +
-    '    } \n' +
-    '} \n' +
-    '</style>\n' +
-    '<div class="markdown-body">\n';
+    // create header
+    let outputHeader =
+        '<style>\n' +
+        css +
+        '.markdown-body { \n' +
+        '    box-sizing: border-box; \n' +
+        '    min-width: 200px; \n' +
+        '    max-width: 980px; \n' +
+        '    margin: 0 auto; \n' +
+        '    padding: 45px; \n' +
+        '} \n' +
+        '\n' +
+        '@media (max-width: 767px) { \n' +
+        '.markdown-body { \n' +
+        '        padding: 15px; \n' +
+        '    } \n' +
+        '} \n' +
+        '</style>\n' +
+        '<div class="markdown-body">\n';
 
-// create footer
-let outputFooter = '</div>\n';
-
-
-let outputContent = outputHeader + outputBody + outputFooter;
+    // create footer
+    let outputFooter = '</div>\n';
 
 
-fs.writeFileSync(outputFile, outputContent);
-console.info('Html written to "' + outputFile + "'");
+    let outputContent = outputHeader + outputBody + outputFooter;
+
+
+    fs.writeFileSync(outputFile, outputContent);
+    console.info('Html written to "' + outputFile + "'");
+}
+
