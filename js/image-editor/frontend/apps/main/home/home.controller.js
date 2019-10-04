e1682e96f20d1c12124c3b6de0992ae8c4a1c8b1
tydaeus <5482721+tydaeus@users.noreply.github.com>
2019-10-03 20:17:55 -0500

    fix: include missed code ncessary for auto-save reduced file functionality

--- js/image-editor/frontend/apps/main/home/home.controller.js
+++ js/image-editor/frontend/apps/main/home/home.controller.js
@@ -7,6 +7,7 @@ const _ = require('underscore');
 const electron = window.require('electron');
 const testMode = electron.remote.getGlobal('testMode');
 const cmdLineParams = electron.remote.getGlobal('cmdLineParams');
+const process = electron.remote.getGlobal('process')
 const fs = window.require('fs');
 const path = require('path');
 
@@ -19,17 +20,21 @@ require('angular')
         ) {
 
             $scope.imageWidth = 1200;
-            $scope.saveName = 'reduced';
+            $scope.saveExtension = 'reduced';
+
+            if (cmdLineParams.length > 0) {
+                console.info('Loading files specified on command-line.');
+                filesPicked(cmdLineParams).then(() => {
+                    $scope.saveAllFilesSameDir();
+                    process.exit();
+                });
+
+            }
 
             $scope.loadFiles = () => {
                 electron.remote.dialog.showOpenDialog({
                     properties: ['openFile', 'multiSelections']}, filesPicked);
             };
-
-            if (cmdLineParams.length > 0) {
-                console.info('Loading files specified on command-line.');
-                filesPicked(cmdLineParams);
-            }
 
             function filesPicked(filePathsArr) {
                 console.info('filePathsArr', filePathsArr);
@@ -47,23 +52,44 @@ require('angular')
 
                 console.info('files', files);
 
-                filesRead(files);
+                return filesRead(files);
+            }
+
+            /**
+             * Uses a file's name to generate its post-reduction name by adding '.reduced' before its extension and
+             * assuming the file will end up in the same dir as the original. E.g. 'C:/temp/foo.jpg' will be renamed to
+             * 'C:/temp/foo.reduced.jpg'.
+             * @param rawFileName full path to file including name and extension
+             * @returns string generated name for post-reduction file
+             */
+            function autoRename(rawFileName) {
+                let fileName = rawFileName.replace(/\\/g, '/');
+                const ext = path.extname(fileName);
+                const dirname = path.dirname(fileName);
+                const basename = path.basename(fileName, ext);
+
+                return path.join(dirname, basename + '.' + $scope.saveExtension + ext);
             }
 
             function filesRead(files) {
+                const promises = [];
 
                 for (let i = 0; i < files.length; i++) {
-                    modifyImage(files[i]);
+                    promises.push(modifyImage(files[i]));
                 }
                 $scope.files = files;
                 console.info(files);
+                console.info('promises', promises);
+
+                return Promise.all(promises);
             }
 
             function modifyImage(file) {
                 let image = new Image();
                 image.src = file.data;
 
-                resizeImage(image, { width: $scope.imageWidth }).then(function(resizedImage) {
+                return resizeImage(image, { width: $scope.imageWidth }).then(function(resizedImage) {
+                    console.info('image resized: ' + file.path);
                     file.imageData = resizedImage.src;
                     $scope.$apply();
                 });
@@ -83,6 +109,16 @@ require('angular')
                 });
             };
 
+            $scope.saveAllFilesSameDir = function () {
+                console.info('Saving all files to their home dirs.');
+
+                _($scope.files).each((file, i) => {
+                    let savePath = autoRename(file.path);
+                    file.saveName = path.basename(savePath);
+                    fs.writeFileSync(savePath, uriToBuffer(file.imageData));
+                });
+            };
+
             // select a directory and save all files there
             $scope.saveAllFiles = function () {
                 // electron: open a dialog to pick a directory to save to
@@ -95,8 +131,8 @@ require('angular')
 
                     _($scope.files).each((file, i) => {
                         const ext = path.extname(file.path);
-                        file.saveName = $scope.saveName + '-' + i;
-                        fs.writeFileSync(path.join(dirName, file.saveName + ext), uriToBuffer(file.imageData));
+                        file.saveName = $scope.saveExtension + '-' + i;
+                        fs.writeFileSync(path.join(dirName, file.saveExtension + ext), uriToBuffer(file.imageData));
                     });
 
                 });
